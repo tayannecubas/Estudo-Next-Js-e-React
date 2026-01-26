a137cfa905ab8a60ef08e205c95a3e16a0e1c16c
tayannecubas <tayannecubas@hotmail.com>
2026-01-26 20:55:08 -0300

    commit inicial

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-gray">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
-          className="dark:invert"
-          src="/next.svg"
+          src="/rainbowcat.gif"
           alt="Next.js logo"
           width={100}
           height={20}
