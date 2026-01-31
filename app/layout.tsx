b1227f1ad957366ea948710441a01d54619e373c
tayannecubas <tayannecubas@hotmail.com>
2026-01-31 20:16:45 -0300

    estudo com video

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,5 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Ninguém Liga",
-  description: "Ninguém merece Netx.Js + React",
-};
 
 export default function RootLayout({
   children,
@@ -25,8 +9,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
+        <h1>Estudo de Next.js + React</h1>
         {children}
       </body>
     </html>
